2919cd507e5852c69a8f9020065b996f90054fbf
esayas <amoragadal@gmail.com>
2024-01-19 19:34:54 +0300

    create-react-project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+      <header>
+        <a href="/">amazonia</a>
+      </header>
+      <main>
+        list products
+      </main>
+    </>
+  );
+}
+
+export default App;
